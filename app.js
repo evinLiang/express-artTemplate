3c8d7962457f2bd6e1ac85544d5337bbfbe5376a
evinLiang <evin@evinweb.com>
2020-12-09 16:38:55 +0800

    content-flash

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-var express = require('express');	//引入express
-var app = express();
-var indexRouter = require('./routes/index');	//引入路由
+const express = require('express');	//引入express
+const session = require('express-session');
+const bodyParser = require('body-parser'); /*post方法*/
+const flash = require('connect-flash');
+const indexRouter = require('./routes/index');	//引入路由
+
+const app = express();
+app.use(bodyParser.json()); // 添加json解析
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// 使用 session 中间件
+app.use(session({
+    secret: 'secrett', // 对session id 相关的cookie 进行签名
+    resave: false,
+    key: 'secrettDb',
+    cookie: {
+        maxAge: 1000 * 60, // 设置 session 的有效时间，单位毫秒
+    },
+    resave: false,
+    saveUninitialized: true,
+}));
+app.use(flash());
 
 app.use('/', indexRouter);//使用路由
 
@@ -8,6 +27,12 @@ app.use('/', indexRouter);//使用路由
 app.engine('html', require('express-art-template'));
 app.set('view options', {
     debug: process.env.NODE_ENV !== 'production'
+});
+
+app.use(function (req, res, next) {
+    res.locals.errors = req.flash('error');
+    res.locals.infos = req.flash('info');
+    next();
 });
 
 //监听3000端口
--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 // '/'的get 路由
-router.get('/', function(req, res, next) {
-  	
-  	//指向index.html模板，并且设置data 
-  	res.render('index.html', {
-		user: {
-			name: 'aui',
-			tags: ['art', 'template', 'nodejs']
-		}
-	});
+router.get('/', function (req, res, next) {
+	res.render('index.html', { errors: req.flash('errors') });
+});
+
+router.post('/', function (req, res, next) {
+	const username = req.body.username
+	const passwork = req.body.passwork
+	if (!username) {
+		req.flash('errors', '用户不能为空');
+		res.redirect('/');
+	} else if (!passwork) {
+		req.flash('errors', '密码不能为空');
+		res.redirect('/');
+	} else {
+		res.render('index.html', { success: '登录成功' });
+	}
+	//指向index.html模板，并且设置data 
+	// req.flash('errors','用户不存在');
+	// res.redirect('/');
+	// res.render('index.html');
 });
 
 module.exports = router;
